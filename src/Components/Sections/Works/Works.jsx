5d15ae9f65cdd3f59432c783cf71657bce1293a5
DimaSleznik <dima.sleznik@gmail.com>
2021-09-27 15:51:59 +0300

    add all projects

--- src/Components/Sections/Works/Works.jsx
+++ src/Components/Sections/Works/Works.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './style.css';
-import work from '../../../img/work_img/portfolio.png';
 import weather from '../../../img/work_img/weather.png';
 import Random from '../../../img/work_img/RandomWords.png';
 import Moon from '../../../img/work_img/moon.png';
@@ -13,7 +12,6 @@ export const Works = ({ setActive }) => {
    const SendToModal = (e) => {
       if (e.target?.nextSibling?.innerHTML) {
          const description = e.target.nextSibling.innerHTML || 'emty';
-         console.log(<div> {description}</div>);
          setActive({ active: true, content: description })
       }
    }
@@ -25,33 +23,94 @@ export const Works = ({ setActive }) => {
                <img src={Random} alt="project" />
                <div className="Work_description">
                   <div className='description_card'>
-                     <h3>Портфолио</h3>
-                     <p>Сайт с Портфолио для работы</p>
+                     <h3>Учебные списки</h3>
+                     <p>Приложение для создание списков , и заучивание слов.
+                        Программа выводит случайной слово из списка.Можно поставить таймер.</p>
                      <h3>Технологии</h3>
                      <ul>
                         <li>React</li>
-                        <li>Html</li>
+                        <li>React-router</li>
                         <li>js</li>
                      </ul>
-                     <a href="http://github.com" target="_blank" rel="noopener noreferrer">Ссылка на гитхаб</a>
+                     <a href="https://github.com/DimaSleznik/RandomWords" target="_blank" rel="noopener noreferrer">Ссылка на гитхаб</a>
                   </div>
                </div>
             </div>
 
             <div className="Works_elem"><img src={weather} alt="project" />
-               <div className="Work_description">Проект портфолио113331</div>
+               <div className="Work_description">
+                  <div className='description_card'>
+                     <h3>Погода</h3>
+                     <p>Приложение с погодой. Использующее  openweathermap API.</p>
+                     <h3>Технологии</h3>
+                     <ul>
+                        <li>React</li>
+                        <li>React-router</li>
+                        <li>Redux</li>
+                     </ul>
+                     <a href="https://github.com/DimaSleznik/Weather_Redux" target="_blank" rel="noopener noreferrer">Ссылка на гитхаб</a>
+                  </div>
+               </div>
             </div>
             <div className="Works_elem"><img src={Chat} alt="project" />
-               <div className="Work_description">Проект портфолио11221</div>
+               <div className="Work_description">
+
+                  <div className='description_card'>
+                     <h3>Чат</h3>
+                     <p>Приложение для групповых чатов. Серверную часть выполняла firebase API.А frontend : React/Redux.</p>
+                     <h3>Технологии</h3>
+                     <ul>
+                        <li>React</li>
+                        <li>React-router</li>
+                        <li>Redux</li>
+                        <li>firebase</li>
+                     </ul>
+                     <a href="https://github.com/DimaSleznik/Chat" target="_blank" rel="noopener noreferrer">Ссылка на гитхаб</a>
+                  </div>
+
+               </div>
             </div>
             <div className="Works_elem"><img src={Moon} alt="project" />
-               <div className="Work_description">Проект портфолио11ss1</div>
+               <div className="Work_description">
+                  <div className='description_card'>
+                     <h3>Планирование</h3>
+                     <p>Планирование задач,с возможностью выбрать дату,время.</p>
+                     <h3>Технологии</h3>
+                     <ul>
+                        <li>js vanilla</li>
+                        <li>html/css</li>
+                     </ul>
+                     <a href="https://github.com/DimaSleznik/Moon_Diary" target="_blank" rel="noopener noreferrer">Ссылка на гитхаб</a>
+                  </div>
+               </div>
             </div>
             <div className="Works_elem"><img src={Todo} alt="project" />
-               <div className="Work_description">Проект портфолио11ss1</div>
+               <div className="Work_description">
+                  <div className='description_card'>
+                     <h3>Тудушка</h3>
+                     <p>Список задачь</p>
+                     <h3>Технологии</h3>
+                     <ul>
+                        <li>React</li>
+                        <li>html/css</li>
+                     </ul>
+                     <a href="https://github.com/DimaSleznik/Moon_Diary" target="_blank" rel="noopener noreferrer">Ссылка на гитхаб</a>
+                  </div>
+               </div>
             </div>
             <div className="Works_elem"><img src={Bar} alt="project" />
-               <div className="Work_description">Проект портфолио11ss1</div>
+               <div className="Work_description">
+                  <div className='description_card'>
+                     <h3>Игра для создания коктейлий</h3>
+                     <p>Игра для вечеринок</p>
+                     <h3>Технологии</h3>
+                     <ul>
+                        <li>Javascript</li>
+                        <li>html/css</li>
+                     </ul>
+                     <a href="https://github.com/DimaSleznik/Tipsy" target="_blank" rel="noopener noreferrer">Ссылка на гитхаб</a>
+                  </div>
+               </div>
             </div>
          </div>
       </div>
